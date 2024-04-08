e3f981bfa8126a128bb2836addaa4d43b6df1993
futurGH <me@futuristick.ca>
2024-04-07 23:10:42 -0400

    Simplify how guides are generated and sorted by moving guide order to a list in config.json

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,7 @@ import { defineCollection, z } from "astro:content";
 
 const guidesCollection = defineCollection({
 	type: "content",
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		category: z.string(),
-		library: z.string(),
-		sort: z.number().optional(),
-	}),
+	schema: z.object({ title: z.string(), description: z.string() }),
 });
 
 export const collections = { guides: guidesCollection };
