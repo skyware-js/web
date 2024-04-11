98428a7242d2b5d830efeeb9df08f157280b3d30
futurGH <me@futuristick.ca>
2024-04-10 22:44:12 -0400

    Bot "Rich Text" and "Embeds and Images" guides

--- src/components/markdown/Reference.tsx
+++ src/components/markdown/Reference.tsx
@@ -1,9 +1,20 @@
+import { ExternalIcon } from "../../assets/icons/ExternalIcon.tsx";
 import { HighlightKind, HighlightText } from "../../lib/rendering/highlight.tsx";
 import { UrlCategoriesReverse } from "../../lib/util/resolveUrl.ts";
 
 export const Reference = (
 	{ to, type, children }: { to: string; type?: string; children?: string },
 ) => {
+	const isExternal = to.startsWith("http");
+	if (isExternal) {
+		return (
+			<HighlightText kind={HighlightKind.Reference} as="a" href={to} className="no-underline">
+				{children}
+				<ExternalIcon className="fill-current w-[0.75em] h-[0.75em]" />
+			</HighlightText>
+		);
+	}
+
 	if (to.startsWith("/")) to = to.slice(1);
 	const [_docs, _package, category, name] = to.split("/");
 
